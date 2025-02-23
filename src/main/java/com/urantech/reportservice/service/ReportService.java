245173f95a5b93521dbd48e4e6e20b74a89841e3
Roman Uranov <uranovrg@gmail.com>
2025-02-23 20:00:14 +0300

    added executor

--- src/main/java/com/urantech/reportservice/service/ReportService.java
+++ src/main/java/com/urantech/reportservice/service/ReportService.java
@@ -8,6 +8,9 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @Service
@@ -16,13 +19,23 @@ public class ReportService {
     private final UserClient userClient;
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    private final ExecutorService executor = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors() - 1);
+
     public void generateAndSendReports() {
         List<UserResponse> users = userClient.getAllUsersWithUnfinishedTasks();
-        for (UserResponse user : users) {
-            String msg = buildMessage(user);
-            kafkaTemplate.send("EMAIL_SENDING", msg);
-            log.info(">>> Message {} sent to EMAIL_SENDING topic: >>>", msg);
-        }
+
+        List<CompletableFuture<Void>> futures = users.stream()
+                .map(user -> CompletableFuture.runAsync(() -> sendReport(user), executor))
+                .toList();
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+    }
+
+    private void sendReport(UserResponse user) {
+        String msg = buildMessage(user);
+        kafkaTemplate.send("EMAIL_SENDING", msg);
+        log.info(">>> Message {} sent to EMAIL_SENDING topic: >>>", msg);
     }
 
     private String buildMessage(UserResponse user) {
